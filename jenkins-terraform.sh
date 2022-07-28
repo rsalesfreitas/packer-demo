260e3c408a291fe47f2e49ffcee400d265ad0f1e
Reginaldo Freitas <rfreitas.sales@gmail.com>
2022-07-28 13:46:48 -0300

    alterando parametros para minha conta

--- jenkins-terraform.sh
+++ jenkins-terraform.sh
@@ -12,5 +12,5 @@ echo "AMI ID: ${AMI_ID}"
 
 echo "writing amivar.tf and uploading it to s3"
 echo 'variable "APP_INSTANCE_AMI" { default = "'${AMI_ID}'" }' > amivar.tf
-S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
+S3_BUCKET='aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3'
 aws s3 cp amivar.tf s3://${S3_BUCKET}/amivar.tf --region $AWS_REGION
